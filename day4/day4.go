ed76806463d8fd19be0c01e525ad5272bd1b514c
IdoKendo <ryuusuke@gmail.com>
2023-12-04 12:02:35 +0200

    feat: solve day 4

--- day4/day4.go
+++ day4/day4.go
@@ -2,11 +2,61 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"os"
+	"strings"
 )
 
+func contains(arr []string, val string) bool {
+	for _, v := range arr {
+		if v == val {
+			return true
+		}
+	}
+	return false
+}
+
 func solution(input string, part int) int {
-	return 0
+	lines := strings.Split(input, "\n")
+	sum := 0
+	pileSize := 0
+	multipliers := make(map[int]int)
+	for i := range lines {
+		multipliers[i] = 1
+	}
+	for i, line := range lines {
+		if line == "" {
+			continue
+		}
+		pileSize += multipliers[i]
+		title := strings.Split(line, ":")
+		numbers := strings.Split(title[1], "|")
+		winningNumbers := numbers[0]
+		hand := numbers[1]
+		numbersList := strings.Split(winningNumbers, " ")
+		handList := strings.Split(hand, " ")
+		matches := -1
+		for _, h := range handList {
+			if h == "" {
+				continue
+			}
+			if contains(numbersList, h) {
+				matches += 1
+			}
+		}
+		if part == 1 {
+			sum += int(math.Pow(2, float64(matches)))
+		} else if matches >= 0 {
+			for j := 1; j < matches+2; j++ {
+				multipliers[i+j] += multipliers[i]
+			}
+		}
+	}
+	if part == 1 {
+		return sum
+	} else {
+		return pileSize
+	}
 }
 
 func main() {
